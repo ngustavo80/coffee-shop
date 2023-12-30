bd3cbae760c4e32a548aa5a14c67d028ceac79dc
kleber costa almeida <ngustavo80@hotmail.com>
2023-12-30 15:43:23 -0300

    Styled Components, Gloval Style and Default Theme

---
+++ styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${props => props.theme['gray-100']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['yellow-800']};
+    border: none;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
